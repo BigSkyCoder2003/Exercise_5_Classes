b05869041e814ed9548048940ede2c9003bbff85
Daniel Lounsbury <lounsbud@oregonstate.edu>
2023-10-30 11:46:27 -0700

    modify the code to reflect the principles from Clean Code, chapters 9, 10 and 12

--- main.rb
+++ main.rb
@@ -1,13 +1,12 @@
 # Exercise 5
 
-class LaunchDiscussionWorkflow
+class DiscussionWorkflow
 
-  def initialize(discussion, host, participants_email_string)
-    @discussion = discussion
-    @host = host
-    @participants_email_string = participants_email_string
-    @participants = []
-  end
+  def initialize(discussion, host, participants)
+		@discussion = discussion
+		@host = host
+		@participants = participants
+	end
 
   # Expects @participants array to be filled with User objects
   def run
@@ -21,22 +20,55 @@ class LaunchDiscussionWorkflow
     end
   end
 
-  def generate_participant_users_from_email_string
+ 
+
+  # ...
+
+end
+
+class User
+	def initialize(email, password)
+		@email = email
+		@password = password
+	end
+
+	def get_user_email()
+		return(@email)
+	end
+
+end
+
+
+class Participants
+
+	def initialize(participants_email_string)
+		@participants_email_string = participants_email_string
+		@participants = []
+		@participants_email_list = []
+	end
+
+
+	def generate_participant_users_from_email_string
     return if @participants_email_string.blank?
     @participants_email_string.split.uniq.map do |email_address|
-      User.create(email: email_address.downcase, password: Devise.friendly_token)
+      @participants <= User.new(email: email_address.downcase, password: Device.friendly_token)
+			@participants_email_list <= @participants[email_address].get_user_email()
     end
   end
 
-  # ...
+	def get_participants()
+		return (@participants_email_list)
+	end
 
 end
 
 
 discussion = Discussion.new(title: "fake", ...)
 host = User.find(42)
-participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
 
-workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-workflow.generate_participant_users_from_email_string
+participants_email_string = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+participants = Participants.new(participants_email_string)
+
+
+workflow = DiscussionWorkflow.new(discussion, host, participants.get_participants())
 workflow.run
